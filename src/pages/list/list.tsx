a995469912a0d304d5c6a225839e3eb4b1409429
王亚征 <yazhengwang@126.com>
2021-12-15 08:53:40 +0800

    list

--- src/pages/list/list.tsx
+++ src/pages/list/list.tsx
@@ -1,31 +1,36 @@
 import React, { FC, useEffect } from 'react'
-import { Table } from 'antd'
+import { Button, Form, Input, Select, Table } from 'antd'
 import { useRequest, useAntdTable } from 'ahooks';
 import Mock from 'mockjs';
 import { ITodo,IColumns } from './typings'
 
 type IData = {
-   data: ITodo[]
+    total: number;
+   list: ITodo[]
 }
+
+const {Option } = Select
 
 function getUsername(): Promise<IData> {
     return new Promise((resolve) => {
       setTimeout(() => {
         resolve(Mock.mock({
-            'data|10': [{
+            'list|10': [{
                 // 属性 id 是一个自增数，起始值为 1，每次增 1
                 'id|+1': 1,
                 'name': '@cname',
                 'email': '@email',
-            }]
+            }],
+            'total': 10,
         }));
       }, 1000);
     });
   }
 
 const List:FC=()=>{
-
+    const [form] = Form.useForm();
     const {data, error, loading } = useRequest(getUsername)
+    const {tableProps, search, params} = useAntdTable(getUsername,{form})
     const columns: IColumns[]=[
         {
             title:'姓名',
@@ -40,13 +45,29 @@ const List:FC=()=>{
         
     },[data])
 
+    const submit=()=>{}
+
     return <div>
             {loading&& <div>loading</div>}
             {error&& <div>error</div>}
-            {data&&Array.isArray(data.data)&&<Table
+            <div style={{ marginBottom: 16 }}>
+                <Form form={form} style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <Form.Item name="gender" initialValue="male">
+                    <Select style={{ width: 120, marginRight: 16 }} onChange={submit}>
+                        <Option value="">all</Option>
+                        <Option value="male">male</Option>
+                        <Option value="female">female</Option>
+                    </Select>
+                    </Form.Item>
+                    <Form.Item name="name">
+                    <Input.Search placeholder="enter name" style={{ width: 240 }} onSearch={submit} />
+                    </Form.Item>
+                </Form>
+                </div>
+            {data&&Array.isArray(data.list)&&<Table
             rowKey="id"
             columns={columns}
-            dataSource={[...data.data]}
+            dataSource={[...data.list]}
         />}
         </div>
 }
